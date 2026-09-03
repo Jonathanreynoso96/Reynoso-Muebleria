7f404caaf0a462c50d71150f087a46fcc4c92fbc
Jonathan Reynoso <jonathan.ruben.reynoso@gmail.com>
2026-09-03 13:54:02 -0600

    Agregar buscador y filtros de productos

--- script.js
+++ script.js
@@ -61,3 +61,65 @@ whatsappFloat.addEventListener("click", function(event) {
 
 });
 });
+
+const botonesFiltro = document.querySelectorAll(".filtro");
+const productos = document.querySelectorAll(".producto");
+const buscador = document.querySelector("#buscarProducto");
+
+let categoriaActual = "todos";
+
+function filtrarProductos() {
+
+    const textoBusqueda = buscador.value.toLowerCase().trim();
+
+    let productosVisibles = 0;
+
+    productos.forEach(producto => {
+
+        const categoria = producto.dataset.categoria;
+        const nombre = producto.querySelector("h3").textContent.toLowerCase();
+
+        const coincideCategoria =
+            categoriaActual === "todos" ||
+            categoria === categoriaActual;
+
+        const coincideBusqueda =
+            nombre.includes(textoBusqueda);
+
+        if (coincideCategoria && coincideBusqueda) {
+            producto.style.display = "block";
+            productosVisibles++;
+        } else {
+            producto.style.display = "none";
+        }
+
+    });
+    
+    const mensajeSinResultados = document.querySelector("#sinResultados");
+
+        if (productosVisibles === 0) {
+            mensajeSinResultados.style.display = "block";
+            console.log("NO HAY RESULTADOS");
+        } else {
+            mensajeSinResultados.style.display = "none";
+}
+}
+
+botonesFiltro.forEach(boton => {
+
+    boton.addEventListener("click", function() {
+
+        categoriaActual = this.dataset.filtro;
+
+        botonesFiltro.forEach(boton => {
+            boton.classList.remove("activo");
+        });
+
+        this.classList.add("activo");
+
+        filtrarProductos();
+    });
+
+});
+
+buscador.addEventListener("input", filtrarProductos);
